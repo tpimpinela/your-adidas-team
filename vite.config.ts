03399322009757dcd9e137632c86afd83a36e104
Tomás Pimpinela Martínez <tom@spimpinela.com>
2023-02-12 11:54:17 +0100

    feat: add postcss nesting plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import postcssNesting from "postcss-nesting";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+  css: {
+    postcss: {
+      plugins: [postcssNesting],
+    },
+  },
+});

d4fee6ea46baaebbb5052a54b5ba4b812c77429c
Tomás Pimpinela Martínez <tom@spimpinela.com>
2023-02-12 12:30:48 +0100

    feat: install and configure react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import router from "./router";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <RouterProvider router={router} />
+      </main>
     </>
   );
 }
---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // TO DO: Main route
+    element: <h1>Hello world</h1>,
+  },
+]);
+
+export default router;

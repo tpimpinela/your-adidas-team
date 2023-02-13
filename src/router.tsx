80af8141a3feec8629924454b4a2a09fbc652103
Tomás Pimpinela Martínez <tom@spimpinela.com>
2023-02-13 18:32:23 +0100

    feat: create player-selection route

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import PlayerSelection from "./components/PlayerSelection";
+import SquadCreator from "./components/SquadCreator";
 
 const router = createBrowserRouter([
   {
     path: "/",
     // TO DO: Main route
     element: <h1>Hello world</h1>,
+  },
+  {
+    path: "/player-selection",
+    element: <SquadCreator />,
+    children: [
+      {
+        path: ":teamId",
+        element: <PlayerSelection />,
+      },
+    ],
   },
 ]);
 

248643f79441efe3c0449d648430fb95d111e13b
Tomás Pimpinela Martínez <tom@spimpinela.com>
2023-02-13 17:04:18 +0100

    feat: create useFetchPlayers custom hook

---
+++ src/hooks/useFetchPlayers.spec.ts
@@ -0,0 +1,47 @@
+import { renderHook, RenderHookResult } from "@testing-library/react";
+import { beforeEach, describe, expect, vi, it } from "vitest";
+import squadMock from "../mocks/squads.mock";
+import { API_ENDPOINTS } from "../utils/config";
+import useFetch from "./useFetch";
+import useFetchPlayers from "./useFetchPlayers";
+
+vi.mock("./useFetch", () => ({
+  default: vi.fn().mockReturnValue({
+    isLoading: false,
+    data: { response: [squadMock] },
+  }),
+}));
+
+describe("useFetchPlayers", () => {
+  let renderHookResult: RenderHookResult<
+    ReturnType<typeof useFetchPlayers>,
+    any
+  >;
+  const teamIdMock = 9;
+
+  beforeEach(() => {
+    renderHookResult = renderHook(() => useFetchPlayers(teamIdMock));
+  });
+
+  it("should call useFetch with correct args", () => {
+    expect(useFetch).toHaveBeenCalledWith(API_ENDPOINTS.squads, {
+      team: teamIdMock,
+    });
+  });
+
+  it("should return mapped players and isLoading", () => {
+    const { id, photo, name, number, position } = squadMock.players[0];
+    expect(renderHookResult.result.current).toStrictEqual({
+      isLoading: false,
+      players: [
+        {
+          id,
+          photo,
+          name,
+          number,
+          position,
+        },
+      ],
+    });
+  });
+});
---
+++ src/hooks/useFetchPlayers.ts
@@ -0,0 +1,37 @@
+import { useMemo } from "react";
+import {
+  FootbalSquadsApiResponse,
+  PlayerMapped,
+} from "../models/squads.models";
+import { API_ENDPOINTS } from "../utils/config";
+import useFetch from "./useFetch";
+
+const useFetchPlayers = (teamId: number) => {
+  const { data, isLoading } = useFetch<FootbalSquadsApiResponse>(
+    API_ENDPOINTS.squads,
+    {
+      team: teamId,
+    }
+  );
+
+  const mappedPlayers: PlayerMapped[] = useMemo(() => {
+    return (
+      data?.response[0].players.map(
+        ({ id, name, number, position, photo }) => ({
+          id,
+          name,
+          number,
+          position,
+          photo,
+        })
+      ) ?? []
+    );
+  }, [data]);
+
+  return {
+    players: mappedPlayers,
+    isLoading,
+  };
+};
+
+export default useFetchPlayers;

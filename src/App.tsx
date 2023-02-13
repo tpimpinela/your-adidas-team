eab71ab6c08174894b78eb2f1af24c50dfa60c09
Tomás Pimpinela Martínez <tom@spimpinela.com>
2023-02-13 20:49:22 +0100

    feat: add MyOwnSquadContextProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import { MyOwnSquadContextProvider } from "./contexts/MyOwnSquad.context";
 import router from "./router";
 
 function App() {
   return (
-    <>
+    <MyOwnSquadContextProvider>
       <Header />
       <main>
         <RouterProvider router={router} />
       </main>
-    </>
+    </MyOwnSquadContextProvider>
   );
 }
 

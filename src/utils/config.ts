7879e76c0d7bf2c06d441373ad2d0676f4215114
Tomás Pimpinela Martínez <tom@spimpinela.com>
2023-02-14 13:11:13 +0100

    feat: add validation model and validation constants

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,5 @@
+import { PlayerPosition } from "../models/squads.models";
+
 export const API_KEY = import.meta.env.VITE_API_KEY;
 export const API_ENDPOINTS = {
   baseUrl: "https://api-football-v1.p.rapidapi.com/v3",
@@ -14,3 +16,12 @@ export const API_ENDPOINTS = {
 export const LEAGUE = 1;
 export const SEASON = 2022;
 export const CACHE_VALIDITY_DAYS = 7;
+export const MAX_SQUAD_PLAYERS = 16;
+export const MIN_SQUAD_POSITIONS = {
+  [PlayerPosition.Coach]: 1,
+  [PlayerPosition.Defender]: 4,
+  [PlayerPosition.Midfielder]: 4,
+  [PlayerPosition.Attacker]: 2,
+  [PlayerPosition.Goalkeeper]: 2,
+};
+export const MAX_PLAYERS_PER_TEAM = 4;
